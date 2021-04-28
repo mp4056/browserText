57f3583b2094490e2bbd5acfda3e897ad9c333b6
Tony Chi <mp4056@gmail.com>
2021-04-28 10:07:45 +0800

    v0.1
    修改RESTful API命名方式，將動詞替換成spring原生標籤後加目標資源名稱

--- src/main/java/com/browserText/controller/browserTextController.java
+++ src/main/java/com/browserText/controller/browserTextController.java
@@ -3,8 +3,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import com.browserText.model.BrowserTextModel;
@@ -22,32 +25,32 @@ public class browserTextController {
 		return "Frontend";
 	}
 
-	@PostMapping(value="/delete/{id}")
+	@DeleteMapping(value="/{id}")
 	public @ResponseBody String deleteBrowserTextById(@PathVariable String id) {
 		service.deleteBrowserTextById(id);
 		return "Frontend";	
 	}
 	
-	@PostMapping(value="/delete/*")
+	@DeleteMapping(value="/*")
 	public @ResponseBody String deleteBrowserTextAll() {
 		service.deleteAll();
 		return "Frontend";
 	}
 	
-	@PostMapping(value="/save")
+	@PutMapping(value="/*")
 	public @ResponseBody String saveBrowserText(BrowserTextModel browserTextModel) {
 		service.saveBrowserText(browserTextModel);
 		return "Frontend";
 	}
 	
-	@PostMapping(value="/find/*")
+	@GetMapping(value="/*")
 	public @ResponseBody String findAllBrowserText(Model model){
 		List<BrowserTextModel>list=service.findAll();
 		model.addAttribute("browserTextList",list);
 		return "Frontend";		
 	}
 	
-	@PostMapping(value="/find/{id}")
+	@GetMapping(value="/{id}")
 	public @ResponseBody String findBrowserTextById(@PathVariable String id,Model model) {
 		List<BrowserTextModel>list=service.findBrowserTextById(id);
 		model.addAttribute("designatedText",list);
