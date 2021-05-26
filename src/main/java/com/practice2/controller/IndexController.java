37ebee20e88e65d854c39a903e4661af12ece24a
keytsai <keytsai513@gmail.com>
2021-05-26 14:45:11 +0800

    add IndexController

---
+++ src/main/java/com/practice2/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.practice2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+  @RequestMapping("/")
+  @Controller
+  public class IndexController {
+
+    @GetMapping
+    String index() {
+      return "redirect:/memos";
+    }
+  }
